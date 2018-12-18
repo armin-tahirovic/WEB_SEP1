211819d9931c11a60d0a7efa6ff8919050aaa91d
armin <armin2013@live.dk>
2018-12-18 15:45:46 +0100

    Second commit

--- org/sep/code/test.js
+++ org/sep/code/test.js
@@ -1,16 +1,61 @@
 var text = "clear";
 var textArray = [];
+var date = "";
+var day;
+var month;
+var year;
+var day_input;
+var month_input;
+var year_input;
+var innerText = "<table><tr><th>Date</th><th>Meter Number</th><th>Channel</th><th>Reading</th><th>Measuretype</th><th>Address</th></tr>";
+
+readUser();
+var brugernavnInfo = "clear";
+var userArray = [];
 
 function readText() {
-    $.get("test3", function (data) {
+    $.get("Text", function (data) {
+
         text = data;
+        date = $("#daterpicker").val().split('-');
+        year = date[0];
+        month = date[1];
+        day = date[2];
 
         textArray = $.csv.toArrays(text);
+        textArray.forEach(line => {
 
-        document.getElementById("text").innerText = textArray[0][2];
+            var localDate = line[0].split('-');
+
+            year_input = localDate[0];
+            month_input = localDate[1];
+            day_input = localDate[2];
+
+            if (day_input === day && month_input === month && year_input === year) {
+                innerText += "<tr>\n" +
+                    "<td>" + line[0] + "</td>\n" +
+                    "<td>" + line[1] + "</td>\n" +
+                    "<td>" + line[2] + "</td>\n" +
+                    "<td>" + line[3] + "</td>\n" +
+                    "<td>" + line[4] + "</td>\n" +
+                    "<td>" + line[7] + line[8] + " <br>" + line[9] + "</td>\n" +
+                    "</tr>";
+            }
+            return innerText;
+        });
+
+        innerText += "</table>\n";
+
+        document.getElementById("text").innerHTML = innerText;
     });
 
-
-
     return false;
+}
+
+function readUser() {
+    $.get("users.txt", function (data) {
+        brugernavnInfo = data;
+
+        userArray = $.csv.toArrays(brugernavnInfo);
+    });
 }
